10a11729cf20058c88160efa710844d55f052b25
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2023-02-10 13:16:51 +0000

    made some changes to post styling

--- blog-space/src/pages/Single.jsx
+++ blog-space/src/pages/Single.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Single() {
-	return <div>Single</div>;
+	return (
+		<div className="single">
+			<div className="content">
+				<img
+					className="samples"
+					src="https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+					alt=""
+				/>
+				<div className="user">
+					<img
+						className="w-10 h-10"
+						src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+						alt=""
+					/>
+					<div className="info">
+						<span>John</span>
+					</div>
+					<p>Posted 2 days ago</p>
+				</div>
+			</div>
+			<div className="menu">m</div>
+		</div>
+	);
 }
 
 export default Single;
